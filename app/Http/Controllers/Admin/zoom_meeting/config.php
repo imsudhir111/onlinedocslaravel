38014942292587a21338b0212d47d8c8849be396
sudhir singh <sudhir.cmw@gmail.com>
2022-05-25 15:48:35 +0530

    zoom meeting link setup
    1. Create app on zoom official website
    2. Dynamically create zoom link for each doctor

---
+++ app/Http/Controllers/Admin/zoom_meeting/config.php
@@ -0,0 +1,5 @@
+<?php
+if (!defined('API_KEY')) define('API_KEY','fW6Pdn4wRN22Wd7i7nZZKw');
+if (!defined('API_SECRET')) define('API_SECRET','oIef7JJPQe3J80nstDXqOoceqKOomohnPEql');
+if (!defined('EMAIL_ID')) define('EMAIL_ID','sudhir.cmw@gmail.com');
+?>
